f6bfcbaa844aabfc12097b3830e6f28ba8128190
Carlos <cagonzalez211@gmail.com>
2022-08-19 11:59:29 -0400

    Added manual way of playing a single round of rock paper scissors

--- Rock_Paper_Scissors/script.js
+++ Rock_Paper_Scissors/script.js
@@ -5,11 +5,37 @@ function getRandomInt(max) {
 function getComputerChoice() {
     randomComputerNum = getRandomInt(3);
     if (randomComputerNum == 0) {
-        return "Rock"
+        return "rock"
     } else if (randomComputerNum == 1) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     };
 };
+
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection == "rock") {
+        if (playerSelection.toLowerCase() == "paper") {
+            return "You Win!"
+        }
+    } else if (computerSelection == "paper") {
+        if (playerSelection.toLowerCase() == "scissors") {
+            return "You Win!"
+        }
+    } else if (computerSelection == "scissors") {
+        if (playerSelection.toLowerCase() == "rock") {
+            return "You Win!"
+        }
+    } else if (computerSelection == playerSelection.toLowerCase()) {
+        return "Tie!"
+    }
+
+    return "You Lose!"
+};
+
+
+let computerSelection = getComputerChoice();
+let playerSelection = "rock"
+
+
 
