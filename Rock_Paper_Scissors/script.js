72fea61e6fc7ba52955efb302b3a0b94046a25c2
Carlos <cagonzalez211@gmail.com>
2022-08-27 22:29:45 -0400

    Added buttons for playing the game. These buttons correspond to a player choice in R,P,S and runs the game

--- Rock_Paper_Scissors/script.js
+++ Rock_Paper_Scissors/script.js
@@ -5,32 +5,26 @@ function getRandomInt(max) {
 function getComputerChoice() {
     randomComputerNum = getRandomInt(3);
     if (randomComputerNum == 0) {
-        return "rock"
+        return "rock";
     } else if (randomComputerNum == 1) {
-        return "paper"
+        return "paper";
     } else {
-        return "scissors"
+        return "scissors";
     };
 };
 
 function playRound(computerSelection, playerSelection) {
-    if (computerSelection == "rock") {
-        if (playerSelection.toLowerCase() == "paper") {
-            return "You Win!"
-        }
-    } else if (computerSelection == "paper") {
-        if (playerSelection.toLowerCase() == "scissors") {
-            return "You Win!"
-        }
-    } else if (computerSelection == "scissors") {
-        if (playerSelection.toLowerCase() == "rock") {
-            return "You Win!"
-        }
+    if (computerSelection == "rock" && playerSelection.toLowerCase() == "paper") {
+        return "You Win!";
+    } else if (computerSelection == "paper" && playerSelection.toLowerCase() == "scissors") {
+        return "You Win!";
+    } else if (computerSelection == "scissors" && playerSelection.toLowerCase() == "rock") {
+        return "You Win!";
     } else if (computerSelection == playerSelection.toLowerCase()) {
-        return "Tie!"
-    }
+        return "Tie!";
+    };
 
-    return "You Lose!"
+    return "You Lose!";
 };
 
 function addPoint(stringResult) {
@@ -38,34 +32,47 @@ function addPoint(stringResult) {
         playerScore++
     } else if (stringResult == "You Lose!") {
         computerScore++
-    }
+    };
 };
 
 function endOfGame() {
     if (playerScore > computerScore) {
-        return "You win the game!"
+        return "You win the game!";
     } else {
-        return "You lost the game!"
-    }
+        return "You lost the game!";
+    };
 }; 
 
 function game() {
     playerScore = 0;
     computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Choose: Rock, Papers, or Scissors")
-        result = playRound(computerSelection, playerSelection)
-        console.log(result)
-        addPoint(result)
-    };
-    console.log(endOfGame())
+    // for (let i = 0; i < 5; i++) {
+    //     playerSelection = prompt("Choose: Rock, Papers, or Scissors")
+    //     result = playRound(computerSelection, playerSelection)
+    //     console.log(result)
+    //     addPoint(result)
+    // };
+    console.log(endOfGame());
 };
 
-
-let computerSelection = getComputerChoice();
+let computerSelection;
 let playerSelection;
-let playerScore = 0;
-let computerScore = 0;
+let playerScore;
+let computerScore;
+
+const choiceButtons = document.querySelectorAll('.selection')
+choiceButtons.forEach(choice => {
+    choice.addEventListener('click', () => {
+        computerSelection = getComputerChoice();
+        playerSelection = choice.textContent.toLowerCase()
+        console.log(`Computer chose ${computerSelection}`);
+        console.log(`Player chose ${playerSelection}`);
+        console.log(playRound(computerSelection,playerSelection));
+    })
+});
+
+
+
 
 
 
