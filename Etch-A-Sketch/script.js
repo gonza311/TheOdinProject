2c8143e09ffb4a73bf03d5d7adcda27f32b6994c
Carlos <cagonzalez211@gmail.com>
2022-08-29 21:06:10 -0400

    Added an eventHandler to button that creates a new grid

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,7 +1,6 @@
 const divGrid = document.querySelector('.divGrid');
 let dimension = 16;
 
-
 function createGrid(dim) {
     for (let i = 0; i < dim; i++) {
         let row = document.createElement('div')
@@ -16,17 +15,34 @@ function createGrid(dim) {
     }
 };
 
+function deleteGrid() {
+    
+}
+
+createGrid(dimension);
+
 function addHoveredClass(element) {
     element.classList.add('hovered')
 };
-
-
-createGrid(dimension);
 
 const squares = document.querySelectorAll('.gridSquare')
 squares.forEach(square => {
     square.addEventListener('mouseover', () => {
         addHoveredClass(square)
     })
+})
+
+const newGrid = document.querySelector('button')
+newGrid.addEventListener('click', () => {
+    let newDimChosen = false;
+    while (newDimChosen != true) {
+        let newDim = prompt('Please choose a new grid size. Max = 100')
+        if (newDim <= 100) {
+            newDimChosen = true;
+            createGrid(newDim)
+        } else {
+            alert('Please choose a number less that 100')
+        }
+    }
 })
 
